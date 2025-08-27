c09a9b9d23ffae8c716a19b484cadec1fbafcb38
ChandraPratap <chandrapratap0001@gmail.com>
2025-08-28 02:03:17 +0530

    changed next.config.js

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   api: {
+    bodyParser: {
+      sizeLimit: "20mb", // increase limit
+    },
+  },
   eslint:{
     ignoreDuringBuilds:true
   }
